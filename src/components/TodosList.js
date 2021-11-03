582b91b9c08b5ac25b2c5e47bc4691ae7b4051a3
Niko Escobal <niko.escobal@gmail.com>
2021-11-03 16:51:47 +0800

    Add on change event

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -9,7 +9,7 @@ class TodosList extends React.Component {
       <ul>
         {this.props.todos.map((todo) => (
           // <li key={todo.id}>{todo.title}</li>
-          <TodoItem key={todo.id} todo={todo} />
+          <TodoItem key={todo.id} todo={todo} handleChangeProps={this.props.handleChangeProps} />
         ))}
       </ul>
     );
